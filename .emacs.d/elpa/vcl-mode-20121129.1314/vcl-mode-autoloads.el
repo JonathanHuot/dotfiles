7705a7fac183cbbb8b1daf606f4984d34bdaa9b2
Jonathan Huot <jonathan.huot@gmail.com>
2020-04-20 14:48:39 +0200

    Initial/Moved .emacs.d to vcsh

---
+++ .emacs.d/elpa/vcl-mode-20121129.1314/vcl-mode-autoloads.el
@@ -0,0 +1,22 @@
+;;; vcl-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "vcl-mode" "vcl-mode.el" (21641 46027 755502
+;;;;;;  913000))
+;;; Generated autoloads from vcl-mode.el
+
+(autoload 'vcl-mode "vcl-mode" "\
+Mode for Varnish Command Language
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; vcl-mode-autoloads.el ends here
